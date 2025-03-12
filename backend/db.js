89f1c1bbdf8c3ce70d7dadb558b08dc2f90513e1
IceWater1000 <vincejerby@gmail.com>
2025-03-12 09:28:57 +0800

    Changed Announcements Location on Home

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,19 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 // database conetction
 const db = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'bis',
-  });
+  host: "127.0.0.1",
+  user: "root",
+  password: "",
+  database: "bis",
+});
 
-  // test connection
-  db.connect((err) => {
-    if (err) {
-      console.error('Error connecting to the database:', err.message);
-      process.exit(1); // Stop server if DB connection fails
-    }
-    console.log('Connected to the database.');
-  });
-  
-  module.exports = db;
+// test connection
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database:", err.message);
+    process.exit(1); // Stop server if DB connection fails
+  }
+  console.log("Connected to the database.");
+});
+
+module.exports = db;
